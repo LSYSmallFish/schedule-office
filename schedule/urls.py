c0d77122074a4d2962149f1f20dea198907c8c2c
LSYSmallFish <332547960@qq.com>
2020-03-12 17:53:01 +0800

    静态文件添加

--- schedule/urls.py
+++ schedule/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from runtest import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('test/', views.TemplateView),
 ]
